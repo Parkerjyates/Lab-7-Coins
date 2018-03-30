dcdea37e176ec48392e26b42d5369283dd5ab90b
Parkerjyates <37162028+Parkerjyates@users.noreply.github.com>
2018-03-29 20:02:06 -0400

    Add files via upload

---
+++ Lab 7/code/game_levels.js
@@ -0,0 +1,21 @@
+var GAME_LEVELS = [
+  ["                                                                                                                 ",
+   "                                                                                                                 ",
+   "                                                                                                                 ",
+   "                    p                                                      x   xx xxx xxx xx  x   x              ",
+   "                                                                           x   x  x x  x  x x x   x              ",
+   "                                                                    oo      x  xx xxx xxx x  xx  x               ",
+   "                                                                    xx       x                  x                ",
+   "            x                                                 xx              xppppppppppppppppx                 ",
+   "  o                                                   xx                       xppppppppppppppx                  ",
+   "  x       x                                  xx                                 xppppppppppppx                   ",
+   "                   o o                o                                          xppppppppppx                    ",
+   "         x        xxxxx      xx      xx                                           xxxxxxxxxx                     ",
+   "                  x!!!x                                                                                          ",
+   "   @              x!!!x                                                                                          ",
+   "xxxxxxxxxxxxxxxxxxx!!!xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"],
+
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
